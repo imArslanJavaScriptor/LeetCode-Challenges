b10510879d2e27dbeb2bb4e048643ba3075e4a6b
Muhammad Arslan <imarslan7061@gmail.com>
2025-01-14 08:29:51 +0500

    Solve 6TH Problem on Leetcode

--- 30JavaScriptProblems.js
+++ 30JavaScriptProblems.js
@@ -105,3 +105,28 @@ let newFunction = (val, index) => val * 10 + index
 
 let transformedArray = map(newArray, newFunction)
 console.log(transformedArray)
+
+//Q#5: Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+
+// The fn function takes one or two arguments:
+
+// arr[i] - number from the arr
+// i - index of arr[i]
+// filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+
+// Please solve it without the built-in Array.filter method.
+
+function filterArray(arr, fn) {
+    const filteredArr = []; // Initialize an empty array to store the results.
+    for (let i = 0; i < arr.length; i++) { // Iterate over the array.
+        if (fn(arr[i], i)) { // Call the fn function with arr[i] and i.
+            filteredArr.push(arr[i]); // If truthy, add arr[i] to filteredArr.
+        }
+    }
+    return filteredArr; // Return the result.
+}
+const arr = [1, 2, 3, 4];
+const fn = (value) => value % 2 === 0; // Filter even numbers.
+
+let result = filterArray(arr, fn)
+console.log(result)
